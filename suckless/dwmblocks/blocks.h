469476257caa7502bcb0ca8d69096ccc1a9dcc45
Sols-thiem <71180005+Sols-thiem@users.noreply.github.com>
2022-07-09 13:43:59 +0000

    put the suckless utils in one folder for convenience

---
+++ suckless/dwmblocks/blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+       
+      //{" ", "~/git/dwmblocks/scripts/",                             ,                           },
+      
+	{" ", "~/git/dwmblocks/scripts/battery",		        5,		             3},
+
+	{" ", "~/git/dwmblocks/scripts/clock",                      60,                           1},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = ' ';
